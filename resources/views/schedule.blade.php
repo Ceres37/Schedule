4bce31bf66fbccbb3ab55772de2e413ce9587e8b
Орлов Александр Владиславович <Ceres@iat.iat>
2018-10-10 17:07:44 +0800

    first commit

---
+++ resources/views/schedule.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.MainLayout')
+@section('con')
+    <div class="col-lg-12">
+
+
+
+    </div>
+    <div class="container">
+        <div>
+            <select class="form-control form-control-lg-10" type="text" placeholder=".form-control-lg">
+                <option value="1">Первый курс</option>
+                <option value="2">Второй курс</option>
+                <option value="3">Третий курс</option>
+                <option value="4">Четвертый курс</option>
+            </select>
+        </div>
+    </div>
+    @endsection
