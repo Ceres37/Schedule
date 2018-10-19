d7004a6d8aabce0627961024ccfa0a600cf54a9f
Ceres <sever.git@maiol.ru>
2018-10-19 16:47:59 +0800

    lolklk

--- resources/views/layouts/MainLayout.blade.php
+++ resources/views/layouts/MainLayout.blade.php
@@ -6,17 +6,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Hello, world!</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="{{route('schedule')}}">Расписание ИАТ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link bg-success" href="#" style="color: white; border-radius: 5px; margin-right: 5px ">
+                    1 курс <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link bg-success" href="#" style="color: white; border-radius: 5px; margin-right: 5px">
+                    2 курс<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link bg-success " href="#" style="color: white; border-radius: 5px; margin-right: 5px">
+                    3 курс <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link bg-success " href="#" style="color: white; border-radius: 5px; margin-right: 5px">
+                    4 курс<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link bg-success" href="#" style="color: white; border-radius: 5px; margin-right: 5px">
+                    5 курс<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link bg-success dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                   style="color: white; border-radius: 5px;">
+                    Преподаватели
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Жданкин Е.В</a>
+                    <a class="dropdown-item" href="#">Жданкин Е.В</a>
+                    <a class="dropdown-item" href="#">Жданкин Е.В</a>
+                </div>
+            </li>
+            {{--<div  class = "date-group date"  data-provide = "datepicker navbar fixed-bottom" style="margin-left: 50px" >--}}
+            {{--<input  type = "text"  class = "form-control" >--}}
+            {{--<div  class = "input-group-addon" >--}}
+            {{--<span  class = " glyphicon glyphicon-th " > </span >--}}
+
+            {{--</div >--}}
+            {{--</div >--}}
+        </ul>
+
+    </div>
+</nav>
 @yield('con')
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </body>
--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,42 +1,131 @@
 @extends('layouts.MainLayout')
 @section('con')
     <div class="col-lg-12">
-
-            <nav class="navbar navbar-dark  bg-primary">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar" aria-controls="exCollapsingNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                    &#9776;
-                </button>
-                <div class="col-lg-10">
-                <div class="collapse" id="exCollapsingNavbar">
-                    <div class="bg-inverse p-a-1">
-                        <ul class="nav navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Привет <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Рассписание</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Шаблон</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Печать</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                </div>
-            </nav>
-
+        <table class="table table-reflow table-hover table-bordered table-sm">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>ПКС-16-1</th>
+                <th>ПКС-16-2</th>
+                <th>ПКС-16-3</th>
+                <th>КС-16-1</th>
+                <th>КС-16-2</th>
+                <th>КС-16-3</th>
+                <th>КС-16-3</th>
+                <th>КС-16-3</th>
+                <th>КС-16-3</th>
+                <th>КС-16-3</th>
+                <th>КС-16-3</th>
+                <th>КС-16-3</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+                <td>Исторя</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
-    <div class="container">
-        <div>
-            <select class="form-control form-control-lg-10" type="text" placeholder=".form-control-lg">
-                <option value="1">Первый курс</option>
-                <option value="2">Второй курс</option>
-                <option value="3">Третий курс</option>
-                <option value="4">Четвертый курс</option>
-            </select>
-        </div>
-    </div>
-    @endsection
+@endsection
